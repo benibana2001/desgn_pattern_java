3a4e72118b8d851818ed9e127476be5e50c793a5
benibana2001 <benibana2001@gmail.com>
2019-10-18 06:11:46 +0900

    [Adapter]edit

--- Adapter/src/FileProperties.java
+++ Adapter/src/FileProperties.java
@@ -1,2 +1,27 @@
-public class FileProperties {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
+public class FileProperties extends Properties implements FileIO{
+    @Override
+    public void readFromFile(String filename) throws IOException {
+        load(new FileInputStream(filename));
+    }
+
+    @Override
+    public void writeToFile(String filename) throws IOException {
+        store(new FileOutputStream(filename), "# yeah :)");
+    }
+
+    @Override
+    public void setValue(String key, String value) {
+        setProperty(key, value);
+    }
+
+    @Override
+    public String getValue(String key) {
+        return null;
+    }
 }
